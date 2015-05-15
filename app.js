d23f3dbf9ee5a515028f89f206bf0841e77eec6e
Jason Lee <chsienlee@gmail.com>
2015-05-16 03:56:54 +0800

    remove useless routes

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/user');
 
 var app = express();
 
@@ -31,7 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
